ef5cb4a314cf42747a9946a0cfe61318737504fd
Yohan Pesce <ypesce@humanbooster.com>
2020-02-13 09:30:54 +0100

    pauses fonctionnelles

--- pauses.js
+++ pauses.js
@@ -1,8 +1,57 @@
-var CronJob = require('cron').CronJob;
+var cron = require('node-cron');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var cron = require('node-cron');
+ 
+cron.schedule('30 10 * * 1-5', () => {
+    client.login("Njc3MTE3NjYzOTQzODUyMDMz.XkUEOw.2VX1rdpGzd8oBn0Z8PvzseyFXVA").then(() => {
+        console.log("CC bg");
+        var guild = client.guilds.get('664564548132667422');
+        if(guild && guild.channels.get('664564548606361602')){
+            guild.channels.get('664564548606361602').send("@everyone Pause !!!").then(() => client.destroy());
+        } else {
+            console.log("nope");
+            //if the bot doesn't have guild with the id guildid
+            // or if the guild doesn't have the channel with id channelid
+        }
+        client.destroy();
+    });
+}, {
+  scheduled: true,
+  timezone: "Europe/Paris"
+});
+cron.schedule('30 14 * * 1-5', () => {
+    client.login("Njc3MTE3NjYzOTQzODUyMDMz.XkUEOw.2VX1rdpGzd8oBn0Z8PvzseyFXVA").then(() => {
+        console.log("CC bg");
+        var guild = client.guilds.get('664564548132667422');
+        if(guild && guild.channels.get('664564548606361602')){
+            guild.channels.get('664564548606361602').send("@everyone Pause !!!").then(() => client.destroy());
+        } else {
+            console.log("nope");
+            //if the bot doesn't have guild with the id guildid
+            // or if the guild doesn't have the channel with id channelid
+        }
+        client.destroy();
+    });
+}, {
+  scheduled: true,
+  timezone: "Europe/Paris"
+});
 
-var job = new CronJob('30 10 * * 1-5', function () {
-    console.log('You will see this message every second');
-}, null, true, 'Europe/Paris');
-job.start();
+cron.schedule('0 16 * * 1-5', () => {
+    client.login("Njc3MTE3NjYzOTQzODUyMDMz.XkUEOw.2VX1rdpGzd8oBn0Z8PvzseyFXVA").then(() => {
+        console.log("CC bg");
+        var guild = client.guilds.get('664564548132667422');
+        if(guild && guild.channels.get('664564548606361602')){
+            guild.channels.get('664564548606361602').send("@everyone Pause !!!").then(() => client.destroy());
+        } else {
+            console.log("nope");
+            //if the bot doesn't have guild with the id guildid
+            // or if the guild doesn't have the channel with id channelid
+        }
+        client.destroy();
+    });
+}, {
+  scheduled: true,
+  timezone: "Europe/Paris"
+});
